6f89b7f677bc59db4807088d6d2e1c1c1761a3dd
Stanislav Shupilkin <sshupilkin@virtuman.com>
2018-06-09 16:28:29 +0300

    Add methods for create and delete namespaces

--- src/Reindexer/Namespaces/Namespaces.php
+++ src/Reindexer/Namespaces/Namespaces.php
@@ -3,6 +3,7 @@
 namespace Reindexer\Namespaces;
 
 use Reindexer\Entity;
+use Reindexer\Indexes\Index;
 
 class Namespaces extends Entity {
 
@@ -15,6 +16,41 @@ class Namespaces extends Entity {
 
         return $this->client->request(
             'GET',
+            $uri,
+            null,
+            $this->defaultHeaders
+        );
+    }
+
+    public function create(string $database, string $name, array $indexes) {
+        $body = [
+            'name' => $name,
+            'storage' => [
+                'enabled' => true
+            ],
+            'indexes' => []
+        ];
+
+        foreach ($indexes as $index) {
+            if ($index instanceof Index) {
+                $body['indexes'][] = $index->getBody();
+            }
+        }
+        $uri = sprintf('/api/%s/db/%s/namespaces', $this->version, $database);
+
+        return $this->client->request(
+            'POST',
+            $uri,
+            json_encode($body),
+            $this->defaultHeaders
+        );
+    }
+
+    public function delete(string $database, string $name) {
+        $uri = sprintf('/api/%s/db/%s/namespaces/%s', $this->version, $database, $name);
+
+        return $this->client->request(
+            'DELETE',
             $uri,
             null,
             $this->defaultHeaders

301587af9e8a36b99cb63902f8635ebc90d25d25
Smolevich <smolevich90@gmail.com>
2020-08-14 11:35:41 +0300

    Add new test case

--- tests/Unit/Reindexer/Services/NamespacesTest.php
+++ tests/Unit/Reindexer/Services/NamespacesTest.php
@@ -5,6 +5,12 @@ use Reindexer\Services\Namespaces;
 use Tests\Unit\Reindexer\BaseTest;
 
 class NamespacesTest extends BaseTest {
+
+    /**
+     * @var Namespaces
+     */
+    private $service;
+
     public function setUp() {
         $this->api = $this->createApiMock(['request']);
         $this->service = new Namespaces($this->api);
@@ -31,6 +37,41 @@ class NamespacesTest extends BaseTest {
         $response->method('getResponseBody')->willReturn(json_encode($responseData));
         $this->api->method('request')->willReturn($response);
         $actual = $this->service->getList();
+        $this->assertEquals(json_encode($responseData), $actual->getResponseBody());
+    }
+
+    /**
+     * @covers \Reindexer\Services\Namespaces
+     */
+    public function testGet() {
+        $responseData = [
+            'name' => 'namespace_one',
+            'storage' => [
+                'enabled' => true
+            ],
+            'indexes' => [
+                [
+                    "name" => "id",
+                    "field_type" => "int",
+                    "index_type" => "hash",
+                    "is_pk" => true,
+                    "is_array" => false,
+                    "is_dense" => true,
+                    "is_sparse" => false,
+                    "collate_mode" => "none",
+                    "sort_order_letters" => "",
+                    "expire_after" => 0,
+                    "config" => [],
+                    "json_paths" => [
+                        "id"
+                    ]
+                ]
+            ]
+        ];
+        $response = $this->createApiResponseMock(['getResponseBody']);
+        $response->method('getResponseBody')->willReturn(json_encode($responseData));
+        $this->api->method('request')->willReturn($response);
+        $actual = $this->service->get('namespace_one');
         $this->assertEquals(json_encode($responseData), $actual->getResponseBody());
     }
 }
